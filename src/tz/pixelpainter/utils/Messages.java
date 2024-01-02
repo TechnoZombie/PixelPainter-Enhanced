67a7aee82d6443f43c949f2ba9a71891028d6a60
TechnoZombie <124879219+TechnoZombie@users.noreply.github.com>
2024-01-02 22:06:18 +0000

    v.0.0.3.2

--- src/tz/pixelpainter/utils/Messages.java
+++ src/tz/pixelpainter/utils/Messages.java
@@ -7,17 +7,19 @@ public class Messages {
         System.out.println("| Welcome to PixelPainter!     |");
         System.out.println("+------------------------------+");
         System.out.println("| Use WASD to move the cursor. |");
+        System.out.println("| KEY E: ERASE SQUARE          |");
         System.out.println("+------------------------------+");
-        System.out.println("| CHOOSE YOUR COLOR!           |");
+        System.out.println("| COLOR SELECTION:             |");
         System.out.println("| KEY 1: BLACK                 |");
         System.out.println("| KEY 2: RED                   |");
         System.out.println("| KEY 3: GREEN                 |");
         System.out.println("| KEY 4: BLUE                  |");
         System.out.println("| KEY 5: YELLOW                |");
+        System.out.println("+------------------------------+");
+        System.out.println("| FILE:                        |");
         System.out.println("| KEY Z: SAVE IMAGE            |");
         System.out.println("| KEY X: LOAD IMAGE            |");
         System.out.println("| KEY C: EXPORT IMAGE          |");
-        System.out.println("| KEY E: ERASE SQUARE          |");
         System.out.println("+------------------------------+");
     }
 }
